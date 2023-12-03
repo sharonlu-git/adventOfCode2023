85a1ff71ef7a17c7219646b57bdcd3c22b70f564
Sharon Lu <sharon.lu@gmail.com>
2023-12-03 15:11:15 -0500

    Day 1 Complete

--- day1/day1.py
+++ day1/day1.py
@@ -18,22 +18,63 @@ for line in lines:
                 lastDigit = line[i]
     total += int(firstDigit + lastDigit)
 
-print(total)
+# print(total)
 
 ## Part 2:
-spellings = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "night": 9,
-}
-for line in lines:
-    firstDigit = "0"
-    lastDigit = "0"
-    for i in range(len(line)):
 
+spellings = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+total = 0
+for line in lines:
+    firstDigitIndex = len(line)
+    lastDigitIndex = 0
+
+    # Get index of first character that is a digit
+    for i in range(len(line)):
+        if line[i].isdigit():
+            firstDigitIndex = i
+            firstDigit = line[i]
+            lastDigitIndex = i
+            break
+    
+    # Get index of last character that is a digit
+    for i in range(len(line)-1, firstDigitIndex-1, -1):
+        if line[i].isdigit():
+            lastDigitIndex = i
+            lastDigit = line[i]
+            break
+    if firstDigitIndex > 2:
+        for spelling in spellings:
+            currentIndex = line[0:len(line)].find(spelling)
+            if currentIndex != -1:
+                if currentIndex < firstDigitIndex:
+                    firstDigitIndex = currentIndex
+                    firstDigit = spellings[spelling]
+                if currentIndex > lastDigitIndex:
+                    lastDigitIndex = currentIndex
+                    lastDigit = spellings[spelling]
+
+    # Search for spelling in last substring:
+    if lastDigitIndex < len(line) - 3:
+        lastDigitNotFound = 0
+        while (lastDigitNotFound == 0):
+            lastDigitNotFound = 1
+            for spelling in spellings:
+                currentIndex = line[lastDigitIndex + 1: len(line)].find(spelling)
+                if currentIndex != -1:
+                    lastDigitIndex = lastDigitIndex + 1 + currentIndex
+                    lastDigit = spellings[spelling]
+                    lastDigitNotFound = 0
+    
+    total += int(firstDigit + lastDigit)
+print(total)
